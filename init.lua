e404dc19caada87b9a7149d9425466a8c649121f
cheyao <94928179+cheyao@users.noreply.github.com>
2024-11-17 16:07:58 +0100

    Update

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.nvchad_theme = "catppuccin"
+vim.g.transparency = true
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,11 +1,9 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
-
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+	theme = "catppuccin",
+	transparency = true
 }
 
 return M
